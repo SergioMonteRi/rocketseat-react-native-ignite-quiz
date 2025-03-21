c832846072057cfcbb532973ad29fac2980e6d12
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-03-21 12:27:05 -0300

    feat: animation quiz question overlay

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -28,6 +28,7 @@ import Animated, {
 import { ProgressBar } from "../../components/ProgressBar";
 import { THEME } from "../../styles/theme";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { OverlayFeedback } from "../../components/OverlayFeedback";
 
 interface Params {
   id: string;
@@ -46,6 +47,7 @@ export function Quiz() {
   const [alternativeSelected, setAlternativeSelected] = useState<null | number>(
     null
   );
+  const [statusReply, setStatusReply] = useState(0)
 
   const { navigate } = useNavigation();
 
@@ -92,8 +94,10 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      setStatusReply(1)
       setPoints((prevState) => prevState + 1);
     } else {
+      setStatusReply(2)
       shakeAnimation();
     }
 
@@ -217,6 +221,8 @@ export function Quiz() {
 
   return (
     <View style={styles.container}>
+      <OverlayFeedback status={statusReply}/>
+      
       <Animated.View style={fixedProgressBarStyle}>
         <Text style={styles.title}>{quiz.title}</Text>
 
